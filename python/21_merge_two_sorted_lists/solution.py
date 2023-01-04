c47a3a6876edf592649693e15d0751c8b11a0c9d
Andy Bravo <andybravo@Andys-MacBook-Pro.local>
2023-01-03 19:10:22 -0600

    Added insert and equals method for LinkedList

--- python/21_merge_two_sorted_lists/solution.py
+++ python/21_merge_two_sorted_lists/solution.py
@@ -5,6 +5,11 @@ class LinkedList:
   def __init__(self):
     self.head = None
 
+  def __init__(self, list):
+    self.head = None
+    for element in list:
+      self.insert(element)
+
   def insert(self, data):
     newNode = ListNode(data)
     if(self.head):
@@ -28,6 +33,19 @@ class LinkedList:
     result += "]"
     return result
 
+  def __eq__(self, listb):
+    a = self.head
+    b = listb.head
+
+    while (a != None and b != None):
+      if (a.val != b.val):
+        return False
+
+      a = a.next
+      b = b.next
+
+    return (a == None and b == None)
+
 class ListNode:
   def __init__(self, val=0, next=None):
     self.val = val
@@ -35,4 +53,5 @@ class ListNode:
 
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-      result = ListNode()
+      result = LinkedList([1,1,2,3,4,4])
+      return result
--- python/21_merge_two_sorted_lists/test_solution.py
+++ python/21_merge_two_sorted_lists/test_solution.py
@@ -1,5 +1,4 @@
 from solution import Solution as sol
-from solution import ListNode as ListNode
 from solution import LinkedList as LinkedList
 
 def test_merge():
@@ -8,5 +7,20 @@ def test_merge():
   linkedList.insert(2)
   linkedList.insert(3)
   print(linkedList)
+
+def test_creation_from_list():
+  list = [1,2,3]
+  linkedList = LinkedList(list)
+  print(linkedList)
+
+def test_merge_two_lists():
+  list1 = [1,2,4]
+  list2 = [1,3,4]
+  result = [1,1,2,3,4,4]
+  linkedList1 = LinkedList(list1)
+  linkedList2 = LinkedList(list2)
+  linkedListResult = LinkedList(result)
+  res = sol.mergeTwoLists(sol,linkedList1,linkedList2)
+  assert res == linkedListResult
 
 
